10042ee63da030db705aaed5d2478fb807f6f65c
SridharanRajesh <136879368+SridharanRajesh@users.noreply.github.com>
2023-06-17 11:37:21 +0530

    Add files via upload

---
+++ demo1/src/main/java/com/example/springdemo/SdemoApplication.java
@@ -0,0 +1,14 @@
+package com.example.springdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SdemoApplication.class, args);
+		System.out.print("sri");
+	}
+
+}
